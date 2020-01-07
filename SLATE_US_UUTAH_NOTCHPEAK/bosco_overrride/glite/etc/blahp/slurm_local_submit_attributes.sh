7c352de2f2aaee04328a9b74e55578986743626e
Mitchell <35324660+Mansalu@users.noreply.github.com>
2020-01-07 13:48:17 -0700

    update resources for notch

--- SLATE_US_UUTAH_NOTCHPEAK/bosco_overrride/glite/etc/blahp/slurm_local_submit_attributes.sh
+++ SLATE_US_UUTAH_NOTCHPEAK/bosco_overrride/glite/etc/blahp/slurm_local_submit_attributes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "#SBATCH --partition=notchpeak-osg"
-echo "#SBATCH --ntasks=4"
-echo "#SBATCH --mem=24G"
+echo "#SBATCH --ntasks=8"
+echo "#SBATCH --mem=16G"
 echo "#SBATCH --time=72:00:00"
